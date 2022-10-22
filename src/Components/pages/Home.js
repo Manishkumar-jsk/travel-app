e6bea4a1d9ffc84c504a189ad9b68fd38455e9ae
Manish kumar <Manishsharma65263@gmail.com>
2022-10-22 10:13:25 -0700

    inital commit

---
+++ src/Components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from '../Card'
+import Footer from '../Footer'
+import HeroSection from '../HeroSection'
+
+export default function Home() {
+    return (
+        <div>
+            <HeroSection />
+            <Card />
+            <Footer />
+        </div>
+    )
+}
